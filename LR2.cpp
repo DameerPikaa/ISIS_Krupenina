21112855a5c500352e5095c6e5653564d3686f03
Pikaa <108175097+DameerPikaa@users.noreply.github.com>
2023-09-12 00:15:40 +0300

    Update LR2.cpp
    без юзнейм стд

--- LR2.cpp
+++ LR2.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
 // Функция для выполнения циклического сдвига массива вправо на n элементов
-void cyclicRightShift(vector<int>& arr, int n) {
+void cyclicRightShift(std::vector<int>& arr, int n) {
     int size = arr.size();
     n %= size;  // Убедимся, что n не превышает размер массива
-    vector<int> temp(size);
+    std::vector<int> temp(size);
     for (int i = 0; i < size; i++) {
         temp[(i + n) % size] = arr[i];
     }
@@ -16,125 +14,125 @@ void cyclicRightShift(vector<int>& arr, int n) {
 
 int main() {
     int n;
-    cin >> n;
-    vector<int> Bitstring1(n);
-    vector<int> Bitstring2(n);
+    std::cin >> n;
+    std::vector<int> Bitstring1(n);
+    std::vector<int> Bitstring2(n);
 
     for (int i = 0; i < n; i++) {
-        cin >> Bitstring1[i];
+        std::cin >> Bitstring1[i];
     }
 
     for (int i = 0; i < n; i++) {
-        cin >> Bitstring2[i];
+        std::cin >> Bitstring2[i];
     }
-    cout << "Bitstring1 = ";
-        for (int i = 0; i < n; i++) {
-            cout << Bitstring1[i] << " ";
-        }
-        cout << endl;
+    std::cout << "Bitstring1 = ";
+    for (int i = 0; i < n; i++) {
+        std::cout << Bitstring1[i] << " ";
+    }
+    std::cout << std::endl;
 
-        cout << "not Bitstring1 = ";
-        for (int i = 0; i < n; i++) {
-            cout << !Bitstring1[i] << " ";
-        }
-        cout << endl;
+    std::cout << "not Bitstring1 = ";
+    for (int i = 0; i < n; i++) {
+        std::cout << !Bitstring1[i] << " ";
+    }
+    std::cout << std::endl;
 
-        cout << "Bitstring2 = ";
-        for (int i = 0; i < n; i++) {
-            cout << Bitstring2[i] << " ";
-        }
-        cout << endl;
+    std::cout << "Bitstring2 = ";
+    for (int i = 0; i < n; i++) {
+        std::cout << Bitstring2[i] << " ";
+    }
+    std::cout << std::endl;
 
-        cout << "not Bitstring2 = ";
-        for (int i = 0; i < n; i++) {
-            cout << !Bitstring2[i] << " ";
-        }
-        cout << endl;
+    std::cout << "not Bitstring2 = ";
+    for (int i = 0; i < n; i++) {
+        std::cout << !Bitstring2[i] << " ";
+    }
+    std::cout << std::endl;
 
-        cout << "Bitstring1 or Bitstring2 = ";
-        for (int i = 0; i < n; i++) {
-            cout << (Bitstring1[i] | Bitstring2[i]) << " ";
-        }
-        cout << endl;
+    std::cout << "Bitstring1 or Bitstring2 = ";
+    for (int i = 0; i < n; i++) {
+        std::cout << (Bitstring1[i] | Bitstring2[i]) << " ";
+    }
+    std::cout << std::endl;
 
-        cout << "Bitstring1 xor Bitstring2 = ";
-        for (int i = 0; i < n; i++) {
-            cout << (Bitstring1[i] ^ Bitstring2[i]) << " ";
-        }
-        cout << endl;
+    std::cout << "Bitstring1 xor Bitstring2 = ";
+    for (int i = 0; i < n; i++) {
+        std::cout << (Bitstring1[i] ^ Bitstring2[i]) << " ";
+    }
+    std::cout << std::endl;
 
-        cout << "Bitstring1 and Bitstring2 = ";
-        for (int i = 0; i < n; i++) {
-            cout << (Bitstring1[i] & Bitstring2[i]) << " ";
-        }
-        cout << endl;
+    std::cout << "Bitstring1 and Bitstring2 = ";
+    for (int i = 0; i < n; i++) {
+        std::cout << (Bitstring1[i] & Bitstring2[i]) << " ";
+    }
+    std::cout << std::endl;
 
     int choice;
     while (true) {
-        cin >> choice;
+        std::cin >> choice;
         if (choice == 0) {
             break;
         }
         switch (choice) {
             case 1: {
                 int shift;
-                cin >> shift;
+                std::cin >> shift;
                 cyclicRightShift(Bitstring1, shift);
-                cout << "cycle sdvig " << shift << " Bitstring1" << endl;
+                std::cout << "cycle sdvig " << shift << " Bitstring1" << std::endl;
                 break;
             }
             case 2: {
                 int shift;
-                cin >> shift;
+                std::cin >> shift;
                 cyclicRightShift(Bitstring2, shift);
-                cout << "cycle sdvig " << shift << " Bitstring2" << endl;
+                std::cout << "cycle sdvig " << shift << " Bitstring2" << std::endl;
                 break;
             }
             default:
                 break;
         }
 
-        cout << "Bitstring1 = ";
+        std::cout << "Bitstring1 = ";
         for (int i = 0; i < n; i++) {
-            cout << Bitstring1[i] << " ";
+            std::cout << Bitstring1[i] << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
 
-        cout << "not Bitstring1 = ";
+        std::cout << "not Bitstring1 = ";
         for (int i = 0; i < n; i++) {
-            cout << !Bitstring1[i] << " ";
+            std::cout << !Bitstring1[i] << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
 
-        cout << "Bitstring2 = ";
+        std::cout << "Bitstring2 = ";
         for (int i = 0; i < n; i++) {
-            cout << Bitstring2[i] << " ";
+            std::cout << Bitstring2[i] << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
 
-        cout << "not Bitstring2 = ";
+        std::cout << "not Bitstring2 = ";
         for (int i = 0; i < n; i++) {
-            cout << !Bitstring2[i] << " ";
+            std::cout << !Bitstring2[i] << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
 
-        cout << "Bitstring1 or Bitstring2 = ";
+        std::cout << "Bitstring1 or Bitstring2 = ";
         for (int i = 0; i < n; i++) {
-            cout << (Bitstring1[i] | Bitstring2[i]) << " ";
+            std::cout << (Bitstring1[i] | Bitstring2[i]) << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
 
-        cout << "Bitstring1 xor Bitstring2 = ";
+        std::cout << "Bitstring1 xor Bitstring2 = ";
         for (int i = 0; i < n; i++) {
-            cout << (Bitstring1[i] ^ Bitstring2[i]) << " ";
+            std::cout << (Bitstring1[i] ^ Bitstring2[i]) << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
 
-        cout << "Bitstring1 and Bitstring2 = ";
+        std::cout << "Bitstring1 and Bitstring2 = ";
         for (int i = 0; i < n; i++) {
-            cout << (Bitstring1[i] & Bitstring2[i]) << " ";
+            std::cout << (Bitstring1[i] & Bitstring2[i]) << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
     }
 
     return 0;
